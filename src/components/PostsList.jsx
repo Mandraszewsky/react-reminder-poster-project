7e097476c2e01ed91a7cae73fb84e7ac05f1ceef
Mandraszewsky <mandrah@o2.pl>
2024-12-08 14:11:02 +0100

    event listeners, states with management (conditional content, lifting states up)

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,58 @@
-import Post from './Post'
-import classes from './PostsList.module.css';
+import { useState } from "react";
 
-function PostsList(){
-    return (
-        <ul className={classes.posts}>
-            <Post author="Author #1" body="some body content #1"/>
-            <Post author="Author #2" body="some body content #2"/>
-        </ul>
-    );
+import NewPost from "./NewPost";
+import Post from "./Post";
+import Modal from "./Modal";
+
+import classes from "./PostsList.module.css";
+
+function PostsList() {
+  const [modalIsVisible, setModelIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModelIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function bodyAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  //   let modalContent;
+
+  //   if (modalIsVisible){
+  //     modalContent = (
+  //         <Modal onClose={hideModalHandler}>
+  //           <NewPost
+  //             onBodyChange={bodyChangeHandler}
+  //             onAuthorChange={bodyAuthorHandler}
+  //           />
+  //         </Modal>
+  //     );
+  //   }
+
+  return (
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={bodyAuthorHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Author #2" body="some body content #2" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;

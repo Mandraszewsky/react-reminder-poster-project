f9bc29d81ac4f6388491431c2d81854ca2501fd8
Mandraszewsky <mandrah@o2.pl>
2024-12-08 17:20:24 +0100

    submitting data with actions & loaders

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,43 +1,20 @@
-import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
+  const posts = useLoaderData();
 
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      
-      const response = await fetch("http://localhost:8080/posts");
-      const responseData = await response.json();
-
-      setPosts(responseData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
-
-  function addPostHandler(postData) {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    //setPosts([postData, ...posts]);
-    setPosts((existingPosts) => [postData, ...existingPosts]); // better way
-    //react ensures that state is the latest correct state, even if there is multiple pending state updates
-  }
+  // function addPostHandler(postData) {
+  //   //setPosts([postData, ...posts]);
+  //   setPosts((existingPosts) => [postData, ...existingPosts]); // better way
+  //   //react ensures that state is the latest correct state, even if there is multiple pending state updates
+  // }
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
@@ -45,17 +22,11 @@ function PostsList() {
         </ul>
       )}
 
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
-      )}
-
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-        <p>Loading posts...</p>
-      </div>
       )}
     </>
   );
